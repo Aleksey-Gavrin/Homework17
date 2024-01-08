9fef28ceba5c5f0bc66550d531d570cc6def7491
Aleksey-Gavrin <darkgarvin@gmail.com>
2024-01-08 23:46:45 +0300

    Initial Commit

---
+++ src/test/java/pro/sky/Homework17/Homework17ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
