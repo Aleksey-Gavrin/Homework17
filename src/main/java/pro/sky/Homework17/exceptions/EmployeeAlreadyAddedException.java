723a1d817dde9a3d5b292f61cfa60043ea952b7c
Aleksey-Gavrin <darkgarvin@gmail.com>
2024-01-13 00:53:32 +0300

    12 steps complete

---
+++ src/main/java/pro/sky/Homework17/exceptions/EmployeeAlreadyAddedException.java
@@ -0,0 +1,6 @@
+package pro.sky.Homework17.exceptions;
+
+public class EmployeeAlreadyAddedException extends RuntimeException {
+    public EmployeeAlreadyAddedException() {
+    }
+}
---
+++ src/main/java/pro/sky/Homework17/exceptions/EmployeeStorageIsFullException.java
@@ -0,0 +1,6 @@
+package pro.sky.Homework17.exceptions;
+
+public class EmployeeStorageIsFullException extends RuntimeException {
+    public EmployeeStorageIsFullException() {
+    }
+}
